eb75f3a6fb4ad6908cdc82e0a71e77a579a24baa
yuto90 <solidusgako4087@gmail.com>
2023-04-02 21:47:56 +0900

    電卓プログラム（途中）

--- app.js
+++ app.js
@@ -1,1 +1,83 @@
-console.log("watch mode");
+// 計算式
+var input = "-2+10*10*10*2/2-2";
+// 式の先頭に0を足す
+var addLeadingZero = function (input) { return "0" + input; };
+// かけ算とわり算あ含まれる式をたし算とひき算だけの式に変換する
+var evaluateMathExpression = function (input) {
+    var _loop_1 = function () {
+        var currentOpeIndex;
+        var kakewariflg = false;
+        var kigouIndexList = [];
+        input.split("").forEach(function (elem, index) {
+            if ((elem === "*" || elem === "/") && !kakewariflg) {
+                currentOpeIndex = index;
+                kigouIndexList.push(index);
+                kakewariflg = true;
+            }
+            else if (/[^0-9.]/.test(elem)) {
+                kigouIndexList.push(index);
+            }
+        });
+        // 演算子があるインデックスを回す
+        var result_1;
+        kigouIndexList.forEach(function (elem, index) {
+            // カレント演算子のインデックスの両隣の数字を切り取って演算する
+            if (elem === currentOpeIndex) {
+                var left = Number(input.slice(kigouIndexList[index - 1], elem));
+                var right = Number(input.slice(currentOpeIndex + 1, kigouIndexList[index + 1]));
+                if (input[currentOpeIndex] === "*") {
+                    result_1 = left * right;
+                }
+                else if (input[currentOpeIndex] === "/") {
+                    result_1 = left / right;
+                }
+                // もともとの計算式を上書き
+                input = input.slice(0, kigouIndexList[index - 1] + 1) + result_1 + input.slice(kigouIndexList[index + 1]);
+            }
+        });
+    };
+    // 文字列に*/が含まれていれば
+    while (input.indexOf("*") != -1 || input.indexOf("/") != -1) {
+        _loop_1();
+    }
+    return input;
+};
+var addSubtract = function (input) {
+    var left = 0;
+    var right = 0;
+    var ope = "+";
+    for (var _i = 0, _a = input.split(""); _i < _a.length; _i++) {
+        var elem = _a[_i];
+        if (elem === "+" || elem === "-") {
+            if (ope === "+") {
+                left += right;
+            }
+            else if (ope === "-") {
+                left -= right;
+            }
+            right = 0;
+            ope = elem;
+        }
+        else {
+            right += Number(elem);
+        }
+    }
+    if (ope === "+") {
+        return left + Number(right);
+    }
+    else if (ope === "-") {
+        return left - Number(right);
+    }
+    return left;
+};
+// -から始まる場合先頭に0を足す
+if (input.slice(0, 1) === "-") {
+    input = addLeadingZero(input);
+}
+//かけ算か割り算が含まれていた時は先に計算
+if (input.indexOf("*") != -1 || input.indexOf("/") != -1) {
+    input = evaluateMathExpression(input);
+    console.log(input);
+}
+var result = addSubtract(input);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,88 @@
-console.log("watch mode");
+// 計算式
+let input = "-2+10*10*10*2/2-2";
+
+// 式の先頭に0を足す
+const addLeadingZero = (input: string) => "0" + input;
+
+// かけ算とわり算あ含まれる式をたし算とひき算だけの式に変換する
+const evaluateMathExpression = (input: string): string => {
+  // 文字列に*/が含まれていれば
+  while (input.indexOf("*") != -1 || input.indexOf("/") != -1) {
+    let currentOpeIndex: number;
+    let kakewariflg = false;
+    const kigouIndexList: number[] = [];
+
+    input.split("").forEach((elem: string, index: number) => {
+      if ((elem === "*" || elem === "/") && !kakewariflg) {
+        currentOpeIndex = index;
+        kigouIndexList.push(index);
+        kakewariflg = true;
+      } else if (/[^0-9.]/.test(elem)) {
+        kigouIndexList.push(index);
+      }
+    });
+
+    // 演算子があるインデックスを回す
+    let result: number;
+    kigouIndexList.forEach((elem: number, index: number) => {
+      // カレント演算子のインデックスの両隣の数字を切り取って演算する
+      if (elem === currentOpeIndex) {
+        let left = Number(input.slice(kigouIndexList[index - 1], elem));
+        let right = Number(input.slice(currentOpeIndex + 1, kigouIndexList[index + 1]));
+
+        if (input[currentOpeIndex] === "*") {
+          result = left * right;
+        } else if (input[currentOpeIndex] === "/") {
+          result = left / right;
+        }
+
+        // もともとの計算式を上書き
+        input = input.slice(0, kigouIndexList[index - 1] + 1) + result + input.slice(kigouIndexList[index + 1]);
+      }
+    });
+  }
+
+  return input;
+};
+
+const addSubtract = (input: string): number => {
+  let left = 0;
+  let right = 0;
+  let ope = "+";
+
+  for (const elem of input.split("")) {
+    if (elem === "+" || elem === "-") {
+      if (ope === "+") {
+        left += right;
+      } else if (ope === "-") {
+        left -= right;
+      }
+      right = 0;
+      ope = elem;
+    } else {
+      right += Number(elem);
+    }
+  }
+
+  if (ope === "+") {
+    return left + Number(right);
+  } else if (ope === "-") {
+    return left - Number(right);
+  }
+
+  return left;
+};
+
+// -から始まる場合先頭に0を足す
+if (input.slice(0, 1) === "-") {
+  input = addLeadingZero(input);
+}
+
+//かけ算か割り算が含まれていた時は先に計算
+if (input.indexOf("*") != -1 || input.indexOf("/") != -1) {
+  input = evaluateMathExpression(input);
+  console.log(input);
+}
+
+const result: number = addSubtract(input);
+console.log(result);
